85d86349bf4d042193bda7c9e1ce8038f6dcfdd5
Joseph <josephylee72@gmail.com>
2023-09-09 15:18:40 -0700

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+const nums = [1, 2, 3, 20];
+const max = Math.max(...nums);
